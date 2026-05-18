606944c7cf9f8bf6acfcd8efc84a00adcdc12388
kartavya <kartavya.aggarwal07@gmail.com>
2026-05-18 11:48:17 +0530

    create css layout for calculator
    Created ui for calculator with css, basic functions for operations were added

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+  return a+b;
+}
+
+function subtract(a, b){
+  if(a >= b){
+    return a-b;
+  }else{
+    return b-a;
+  }
+}
+
+function multiply(){
+  return a*b;
+}
+
+function divide(){
+  if(a >= b){
+    return a/b;
+  }else{
+    return b/a;
+  }
+}
+
+function operate(a, b, operator){
+
+}
