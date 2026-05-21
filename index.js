0793e2def564ff5f7df534e71f2a1390b5cca2be
kartavya <kartavya.aggarwal07@gmail.com>
2026-05-21 12:25:38 +0530

    differentiate buttons based on data-sets

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function subtract(a, b) {
   }
 }
 
-function multiply() {
+function multiply(a, b) {
   return a * b;
 }
 
-function divide() {
+function divide(a, b) {
   if (a >= b) {
     return a / b;
   } else {
@@ -26,10 +26,38 @@ const buttonsDiv = document.querySelector('#buttons');
 buttonsDiv.addEventListener('click', (event) => {
   if (event.target.tagName === 'BUTTON') {
     // should I use strict equality? -> Yes you should
-    console.log(event.target.innerText);
+    // console.log(event.target.innerText);
+    let num1 = 0;
+    let num2 = 0;
+    if(event.target.dataset.type == "function"){
+      console.log("function was clicked");
+    }else if(event.target.dataset.type == "operator"){
+      let operator = event.target.innerText;
+      operate(num1, num2, operator);
+    }else{
+      console.log("number was clicked");
+    }
   }
 });
 
-function operate(a, b, operator) {}
+function operate(a, b, operator) {
+  if(operator == '+'){
+    console.log("hello");
+    add(a, b);
+  }else if(operator == '-'){
+    subtract(a, b);
+  }else if(operator == '*'){
+    multiply(a, b);
+  }else if(operator == '/'){
+    divide(a, b);
+  }else if(operator == '='){
+    displayResult();
+  }else if(operator == 'AC'){
+    clearResult();
+  }else{
+    console.log("error");
+  }
+}
 
 function displayResult() {}
+function clearResult() {}
